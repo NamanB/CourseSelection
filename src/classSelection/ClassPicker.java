8779935d97ebaeb592b961d7647c5635b70f4283
sasha2424 <sashaivanov2424@gmail.com>
2017-01-26 21:55:05 -0800

    partial layout made

--- src/classSelection/ClassPicker.java
+++ src/classSelection/ClassPicker.java
@@ -3,10 +3,15 @@ package classSelection;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class ClassPicker {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +41,30 @@ public class ClassPicker {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1185, 705);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.controlHighlight);
+		panel.setBounds(15, 16, 313, 617);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(15, 16, 283, 95);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(15, 52, 253, 26);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblStudentId = new JLabel("Student ID:");
+		lblStudentId.setBounds(15, 16, 253, 20);
+		panel_1.add(lblStudentId);
 	}
 
 }
